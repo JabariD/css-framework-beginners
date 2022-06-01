3e49f62fd9fc9994d9f53a9bf2180cc8a51647d3
Payton Dennis <dennispayton15@gmail.com>
2022-06-01 16:36:45 -0500

    Fix VSCode extension not recognizing Tailwind

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,10 @@
+// Used for Tailwind CSS IntelliSense Extension. 
+// See: https://marketplace.visualstudio.com/items?itemName=bradlc.vscode-tailwindcss
+
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
